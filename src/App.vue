1c4adebea9e262211892cb4bfae8bba7762771d5
giosciafra <francescogioscia8@gmail.com>
2024-02-29 15:38:32 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+
+import AppNav from '../src/components/AppNav';
+
+export default {
+  components: {
+    AppNav,
+  }
+}
+</script>
+
+<template>
+  <AppNav></AppNav>
+</template>
+
+<style></style>
